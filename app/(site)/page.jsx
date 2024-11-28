b221ea4c5863aee08b89422ead98119686dde96d
ThangBuiDuc <thangbd@hpu.edu.vn>
2024-11-28 22:24:03 +0700

    fix

--- app/(site)/page.jsx
+++ app/(site)/page.jsx
@@ -5,7 +5,6 @@ import { Button } from "@nextui-org/button";
 import { Link } from "@nextui-org/react";
 import ArticleSlide from "../_component/articleSlide/ArticleSlide";
 import EventSlide from "../_component/eventSlide/EventSlide";
-
 import { getArticlesData, getEventsData } from "@/utils/funcionApi/select";
 
 export default async function HomePage() {
