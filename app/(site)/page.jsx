85750438d2c796a5173b1617ca16c85d864f0bcc
ThangBuiDuc <thangbd@hpu.edu.vn>
2024-11-28 23:08:44 +0700

    fix h

--- app/(site)/page.jsx
+++ app/(site)/page.jsx
@@ -23,7 +23,7 @@ export default async function HomePage() {
           // width={1920}
           // height={1080}
           // sizes="100vw"
-          style={{ width: "100%", height: "80vw" }}
+          style={{ width: "100%", height: "80vh" }}
           className="object-cover"
           // priority
         />
